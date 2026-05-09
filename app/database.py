faf19f01b0e3c286b5f7f5f57ea39a292bb1ac83
ushiwateam <65821012+ushiwateam@users.noreply.github.com>
2026-05-09 20:19:11 +0100

    Feature/sqlalchemy+user-model (#2)
    * feat: add database postgres file
    
    * feat: add user model
    
    ---------
    
    Co-authored-by: hichamkbiri <ushiwateam@gmail.com>

---
+++ app/database.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+load_dotenv()
+
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
+DB_NAME = os.getenv("POSTGRES_DB")
+
+DATABASE_URL = (
+    f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    pass
