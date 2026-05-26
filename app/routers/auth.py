3f42c16be1b6187b28dcd41ccb0c4966c3728c8c
ushiwateam <65821012+ushiwateam@users.noreply.github.com>
2026-05-26 17:52:53 +0100

    Feature/token fast api (#11)
    * feat : add special fastapi token service
    adjust authenticate_user params
    
    * feat : add token route ( not included in schema )
    
    ---------
    
    Co-authored-by: hichamkbiri <ushiwateam@gmail.com>

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from typing import Annotated
 
 from app.dependencies import get_db
 from app.schemas.user import UserRegister, UserLogin
-from app.services.auth_service import register, login
+from app.services.auth_service import register, login, token_login
 
 router = APIRouter(tags=["Users"])
 
@@ -14,15 +15,24 @@ router = APIRouter(tags=["Users"])
     name="Register a new user"
 )
 def register_route(
-    user: UserRegister,
-    db: Annotated[Session, Depends(get_db)]
+        user: UserRegister,
+        db: Annotated[Session, Depends(get_db)]
 ):
     return register(user, db)
+
 
 @router.post("/login",
              name="Login an existent user")
 def login_route(
-    user: UserLogin,
-    db: Annotated[Session, Depends(get_db)]
+        user: UserLogin,
+        db: Annotated[Session, Depends(get_db)]
 ):
     return login(user, db)
+
+
+@router.post("/token", include_in_schema=False)
+async def login_for_access_token(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+        db: Annotated[Session, Depends(get_db)]
+):
+    return token_login(form_data, db)
