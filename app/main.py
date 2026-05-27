755c2988bb69db5eaf81582e268af91ba1eae563
hichamkbiri <ushiwateam@gmail.com>
2026-05-27 19:32:32 +0100

    feat : add todos router

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.routers.auth import router as auth_router
+from app.routers.todos import router as todos_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(todos_router)
 
 @app.get("/")
 async def root():
