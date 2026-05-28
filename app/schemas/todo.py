fdcd7ead9dbc77fd875f351a8c2023034008fb96
hichamkbiri <ushiwateam@gmail.com>
2026-05-28 19:07:03 +0100

    feat : add get all todos schema

--- app/schemas/todo.py
+++ app/schemas/todo.py
@@ -10,3 +10,11 @@ class TodosOut(BaseModel):
     id: int
     title: str = Field(title="Title", max_length=255)
     description: str | None = Field(title="Description", default=None)
+
+
+class AllTodosOut(BaseModel):
+    data: list[TodosOut]
+    page: int
+    limit: int
+    total: int
+
