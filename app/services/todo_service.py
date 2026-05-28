f69b53cb06f14740b9de6a50fe4993a3405db12d
hichamkbiri <ushiwateam@gmail.com>
2026-05-28 19:07:20 +0100

    feat : add get all todos service

--- app/services/todo_service.py
+++ app/services/todo_service.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select, func
 
 from app.models import User, Todo
-from app.schemas.todo import TodosCreate
+from app.schemas.todo import TodosCreate, AllTodosOut
 
 
 def create_todo(todo: TodosCreate, user: User, db: Session):
@@ -16,3 +17,15 @@ def create_todo(todo: TodosCreate, user: User, db: Session):
     db.refresh(todo)
 
     return todo
+
+
+def get_todos(user: User, db: Session, page, limit):
+    offset = (page - 1) * limit
+    total_todos = db.scalar(select(func.count()).select_from(Todo).where(Todo.user_id == user.id)) or 0
+    data = list(db.scalars(select(Todo).where(Todo.user_id == user.id).offset(offset).limit(limit)).all())
+    return {
+        "data": data,
+        "page": page,
+        "limit": limit,
+        "total": total_todos
+    }
