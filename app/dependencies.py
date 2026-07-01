48cea48a89d6e9709cf16b8b197d585e84491d1d
ushiwateam <65821012+ushiwateam@users.noreply.github.com>
2026-07-01 20:52:02 +0100

    Feature/user repository (#34)
    * feature: add user_repository
    
    * feature: add get_user_repository dep
    
    * feature: replace db with user_repository
    
    * feature: replace db with user_repository
    
    ---------
    
    Co-authored-by: hichamkbiri <ushiwateam@gmail.com>

--- app/dependencies.py
+++ app/dependencies.py
@@ -9,6 +9,7 @@ from fastapi.security import OAuth2PasswordBearer
 from app.config import TOKEN_SECRET_KEY, TOKEN_ALGORITHM
 from app.database import SessionLocal
 from app.models.user import User
+from app.repositories import UserRepository
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -22,8 +23,10 @@ def get_db():
     finally:
         db.close()
 
+DbDep = Annotated[Session, Depends(get_db)]
 
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Annotated[Session, Depends(get_db)]):
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: DbDep):
     credentials_exception = HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST,
         detail="Email inexistant ou mot de passe incorrect"
@@ -43,6 +46,7 @@ def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Annotate
         raise credentials_exception
     return existing_user
 
-# def get_current_user(token: Annotated[str, oauth2_scheme], db: Annotated[Session, Depends(get_db)]):
-#     print(token)
-#     return "token"
+def get_user_repository(db: DbDep):
+    return UserRepository(db)
+
+UserRepositoryDep = Annotated[UserRepository, Depends(get_user_repository)]
