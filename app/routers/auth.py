4a17762390c6fba80c4f2bc84df86071551ad9c3
hichamkbiri <ushiwateam@gmail.com>
2026-07-08 21:22:31 +0100

    feature: add user service exceptions

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
 
 from app.dependencies import UserRepositoryDep
 from app.responses import EMAIL_ALREADY_REGISTERED_RESPONSE, LOGIN_UNAUTHORIZED_RESPONSE
 from app.schemas.user import UserRegister, UserLogin
-from app.services.auth_service import register, login, token_login
+from app.services.auth_service import register, login, token_login, EmailRegistered
+from app.services.exceptions import UnauthorizedUser
 
 router = APIRouter(tags=["Users"])
 
@@ -22,7 +23,13 @@ def register_route(
         user: UserRegister,
         user_repository: UserRepositoryDep
 ):
-    return register(user, user_repository)
+    try:
+        return register(user, user_repository)
+    except EmailRegistered as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=e.detail
+        )
 
 
 @router.post("/login",
@@ -35,7 +42,14 @@ def login_route(
         user: UserLogin,
         user_repository: UserRepositoryDep
 ):
-    return login(user, user_repository)
+    try:
+        return login(user, user_repository)
+    except UnauthorizedUser as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=e.detail
+        )
+
 
 
 @router.post("/token", include_in_schema=False)
