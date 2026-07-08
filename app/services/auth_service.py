82aff19de4939ee25dd3c9ad10d394c0ae594729
ushiwateam <65821012+ushiwateam@users.noreply.github.com>
2026-07-08 21:23:51 +0100

    Feature/auth service exceptions (#36)
    * feature: define user service exceptions
    
    * feature: add user service exceptions
    
    * feature: add user service exceptions
    
    ---------
    
    Co-authored-by: hichamkbiri <ushiwateam@gmail.com>

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,11 +1,11 @@
 from datetime import timedelta
 
-from fastapi import HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.dependencies import UserRepositoryDep
 from app.schemas.user import UserRegister, UserLogin
 from app.config import ACCESS_TOKEN_EXPIRE_HOURS
+from app.services.exceptions import EmailRegistered, UnauthorizedUser
 from app.utils import create_access_token, prepare_token_data, pwd_context
 
 DUMMY_HASH = pwd_context.hash("dummypassword")
@@ -15,10 +15,7 @@ def register(user_data: UserRegister, user_repository: UserRepositoryDep):
     existing_user = user_repository.get_user_by_email(user_data.email)
 
     if existing_user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email already registered"
-        )
+        raise EmailRegistered
 
     hashed_password = pwd_context.hash(user_data.password)
 
@@ -47,10 +44,7 @@ def authenticate_user(email, password, user_repository: UserRepositoryDep):
 def login(user_data: UserLogin, user_repository: UserRepositoryDep):
     existing_user = authenticate_user(user_data.email, user_data.password, user_repository)
     if not existing_user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Email inexistant ou mot de passe incorrect"
-        )
+        raise UnauthorizedUser
     access_token_expires = timedelta(hours=ACCESS_TOKEN_EXPIRE_HOURS)
     token_data = prepare_token_data(existing_user)
     token = create_access_token(token_data, access_token_expires)
@@ -60,10 +54,7 @@ def login(user_data: UserLogin, user_repository: UserRepositoryDep):
 def token_login(form_data: OAuth2PasswordRequestForm, user_repository: UserRepositoryDep):
     existing_user = authenticate_user(form_data.username, form_data.password, user_repository)
     if not existing_user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Email inexistant ou mot de passe incorrect"
-        )
+        raise UnauthorizedUser
     access_token_expires = timedelta(hours=ACCESS_TOKEN_EXPIRE_HOURS)
     token_data = prepare_token_data(existing_user)
     token = create_access_token(token_data, access_token_expires)
