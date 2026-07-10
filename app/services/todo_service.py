bf64e5ac0a858c35145bab364c1fddee50aca8ba
ushiwateam <65821012+ushiwateam@users.noreply.github.com>
2026-07-10 13:15:13 +0100

    Feature/todo service (#39)
    * feature: add todo service
    
    * feature: add todo service dependency
    
    * feature: link todo service dependency to routes
    
    ---------
    
    Co-authored-by: hichamkbiri <ushiwateam@gmail.com>

--- app/services/todo_service.py
+++ app/services/todo_service.py
@@ -1,64 +1,67 @@
-from app.dependencies import TodoRepositoryDep
 from app.models import User
+from app.repositories import TodoRepository
 from app.schemas.todo import TodosCreate, TodosUpdate, TodosPatch
 from app.services.exceptions import UnfoundTodo, AccessUnauthorized
 
+class TodoService:
+    def __init__(self, todo_repository: TodoRepository, user: User):
+        self.todo_repository = todo_repository
+        self.user = user
 
-def create_todo(todo: TodosCreate, user: User, todo_repository: TodoRepositoryDep):
-    return todo_repository.create(
-        title=todo.title,
-        description=todo.description,
-        user_id=user.id
-    )
-
-
-def get_todos(user: User, todo_repository: TodoRepositoryDep, page, limit):
-    offset = (page - 1) * limit
-    total_todos = todo_repository.count_todos_by_user_id(user.id)
-    data = todo_repository.get_all_by_user_id(user.id, offset, limit)
-    return {
-        "data": data,
-        "page": page,
-        "limit": limit,
-        "total": total_todos
-    }
+    def create_todo(self, todo: TodosCreate):
+        return self.todo_repository.create(
+            title=todo.title,
+            description=todo.description,
+            user_id=self.user.id
+        )
 
 
-def get_owned_todo(
-        user: User,
-        todo_repository: TodoRepositoryDep,
-        todo_id: int,
-):
-    existing_todo = todo_repository.get_by_id(todo_id)
-
-    if not existing_todo:
-        raise UnfoundTodo()
-
-    if user.id != existing_todo.user_id:
-        raise AccessUnauthorized()
-
-    return existing_todo
+    def get_todos(self, page, limit):
+        offset = (page - 1) * limit
+        total_todos = self.todo_repository.count_todos_by_user_id(self.user.id)
+        data = self.todo_repository.get_all_by_user_id(self.user.id, offset, limit)
+        return {
+            "data": data,
+            "page": page,
+            "limit": limit,
+            "total": total_todos
+        }
 
 
-def update_todo(user: User, todo_repository: TodoRepositoryDep, todo_id: int, todo: TodosUpdate):
-    existing_todo = get_owned_todo(user, todo_repository, todo_id)
+    def get_owned_todo(
+            self,
+            todo_id: int,
+    ):
+        existing_todo = self.todo_repository.get_by_id(todo_id)
 
-    update_data = todo.model_dump()
+        if not existing_todo:
+            raise UnfoundTodo()
 
-    return todo_repository.update(existing_todo, update_data)
+        if self.user.id != existing_todo.user_id:
+            raise AccessUnauthorized()
 
-
-def patch_todo(user: User, todo_repository: TodoRepositoryDep, todo_id: int, todo: TodosPatch):
-    existing_todo = get_owned_todo(user, todo_repository, todo_id)
-
-    update_data = todo.model_dump(exclude_unset=True)
-
-    return todo_repository.update(existing_todo, update_data)
+        return existing_todo
 
 
-def delete_todo(user: User, todo_repository: TodoRepositoryDep, todo_id: int):
-    existing_todo = get_owned_todo(user, todo_repository, todo_id)
+    def update_todo(self, todo_id: int, todo: TodosUpdate):
+        existing_todo = self.get_owned_todo(todo_id)
 
-    todo_repository.delete(existing_todo)
+        update_data = todo.model_dump()
 
-    return
+        return self.todo_repository.update(existing_todo, update_data)
+
+
+    def patch_todo(self, todo_id: int, todo: TodosPatch):
+        existing_todo = self.get_owned_todo(todo_id)
+
+        update_data = todo.model_dump(exclude_unset=True)
+
+        return self.todo_repository.update(existing_todo, update_data)
+
+
+    def delete_todo(self, todo_id: int):
+        existing_todo = self.get_owned_todo(todo_id)
+
+        self.todo_repository.delete(existing_todo)
+
+        return
