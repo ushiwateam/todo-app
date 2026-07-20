f9b38f2df28344258f97d0797744866061fd7129
hichamkbiri <ushiwateam@gmail.com>
2026-07-20 14:43:14 +0100

    feature: add Todo Domain entity

---
+++ app/domain/entities/todo.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, asdict
+
+
+@dataclass(slots=True)
+class Todo:
+    title: str
+    description: str
+    user_id: int
+    id: int | None = None
+
+    def model_dump(self, exclude_unset=False):
+        if exclude_unset:
+            return {k: v for k, v in asdict(self).items() if v is not None}
+        return asdict(self)
