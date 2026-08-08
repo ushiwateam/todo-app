b4b19edb7e96c046fb2c647a4015582a2c550103
hichamkbiri <ushiwateam@gmail.com>
2026-08-08 21:50:44 +0100

    feature: add get_user_by__id

--- app/data_access/repositories/user_repository.py
+++ app/data_access/repositories/user_repository.py
@@ -1,10 +1,9 @@
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.data_access.database.models import User as ModelUser
 from app.business.entities.user import User as EntityUser
 from app.data_access.repositories.base import ISqlAlchemyRepository
-
-from app.business.entities.user import User
 from app.data_access.interfaces.user import IUserRepository
 from app.data_access.mappers.user import to_model, to_entity
 
@@ -21,11 +20,12 @@ class UserSqlAlchemyRepository(IUserRepository, ISqlAlchemyRepository[EntityUser
     def to_entity(instance: ModelUser) -> EntityUser:
         return to_entity(instance)
 
-    def register_user(self, user: User):
+    def register_user(self, user: EntityUser):
         return self._create(user)
 
     def get_user_by_email(self, email: str) -> EntityUser | None:
         instance = self._get_one_or_none(ModelUser.email == email)
         return self._to_entity_or_none(instance)
 
-
+    def get_user_by_id(self, user_id: int) -> EntityUser | None:
+        return self._to_entity_or_none(self._get_by_id_model(instance_id=user_id))
