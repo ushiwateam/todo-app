943a121d2e1b8a70279797a02cfeae8fd1024fa7
hichamkbiri <ushiwateam@gmail.com>
2026-08-08 21:50:35 +0100

    feature: add get_user_by__id

--- app/data_access/interfaces/user.py
+++ app/data_access/interfaces/user.py
@@ -12,3 +12,8 @@ class IUserRepository(ABC):
     @abstractmethod
     def get_user_by_email(self, email: str) -> User | None:
         pass
+
+
+    @abstractmethod
+    def get_user_by_id(self, user_id: int) -> User | None:
+        pass
